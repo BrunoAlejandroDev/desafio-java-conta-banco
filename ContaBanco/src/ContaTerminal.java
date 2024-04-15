d8ae480a23b78c776a3a81118d8f8f523a5450d8
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-15 12:27:18 -0300

    criação do objeto scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // instânciando o objeto Scanner
+        Scanner scanner = new Scanner(System.in);
+
+
     }
 }
