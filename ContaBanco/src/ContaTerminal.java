2fe03477555c1d4f77aaf7a1cdfc06d86709da95
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-15 12:34:05 -0300

    criação dos inputs do usuário e imprimir valores

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,6 +5,22 @@ public class ContaTerminal {
         // instânciando o objeto Scanner
         Scanner scanner = new Scanner(System.in);
 
+        // criar os imputs para o usuário
+        System.out.println("Digite o número da sua agência: (Ex: 1234)");
+        int agencia = scanner.nextInt();
 
+        System.out.println("Digite a identificação da sua agência: (Ex: 123-4)");
+        String numAgencia = scanner.next();
+
+        System.out.println("Digite o seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        // imprimindo os dados do usuário
+        System.out.println("-------");
+        System.out.println("Olá " + nome + ". Obrigado por criar uma conta em nosso banco, sua agência é" + numAgencia + "com a conta de número" + agencia + " e seu saldo " + saldo + " já está disponível para saque");
+        System.out.println("-------");
     }
 }
